b6aaf8615dcb68b6a0fd3a9cc3af257c99c066aa
Durgesh Bairagi <35696806+DurgeshBairagi@users.noreply.github.com>
2024-09-18 13:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    // Define a lookup table for Roman numerals
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
 
-  //your code here
+    // Initialize the result
+    let roman = '';
+
+    // Loop through the lookup table
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            // Append the corresponding numeral and reduce the number
+            roman += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
+        }
+    }
+
+    return roman;
+}
+  
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
